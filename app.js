89d01fc52e65f7f750b5ebcb10aa17af118ae0c0
Souvik Dey <shoredasherdey680@gmail.com>
2024-08-17 21:54:45 +0530

    cookie probelm resolving 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-navy-five.vercel.app/",
     credentials: true,
     // Replace with your frontend's URL
   })
